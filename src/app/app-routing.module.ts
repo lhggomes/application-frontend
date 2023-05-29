709804d075eae14f9a3f73a371b2b9a43cc144df
Lucas Gomes <lucas.henrique.s.go@gmail.com>
2023-05-28 23:26:43 -0300

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
+import { ListCompanyComponent } from './company/componenets/list-company/list-company.component';
+import { ListSupplierComponent } from './supplier/componenets/list-supplier/list-supplier.component';
+import { CreateCompanyComponent } from './company/componenets/create-company/create-company.component';
+import { CreateSupplierComponent } from './supplier/componenets/create-supplier/create-supplier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', component: HomeComponent },
+  { path: 'company', component: ListCompanyComponent },
+  { path: 'company/:id/edit', component: CreateCompanyComponent },
+  { path: 'company/create', component: CreateCompanyComponent },
+  { path: 'supplier', component: ListSupplierComponent },
+  { path: 'supplier/:id/edit', component: CreateSupplierComponent },
+  { path: 'supplier/create', component: CreateSupplierComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/supplier/services/supplier.ts
+++ src/app/supplier/services/supplier.ts
@@ -9,62 +9,20 @@ import { Observable, throwError, of, catchError } from 'rxjs';
 })
 export class SupplierService {
 
-  private urlApi = 'http://127.0.0.1:8080/api/task'
+  private urlApi = 'http://127.0.0.1:8080/api/supplier'
   private jsonHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
 
   constructor(private http: HttpClient) { }
 
-  obterTarefas(): Observable<Tarefa[]> {
-    const response = this.http.get<Tarefa[]>(this.urlApi);
-    return response;
-  }
+  
 
-  criarTarefa(tarefa: Tarefa) {
-
-    return this.http.post<Tarefa>(this.urlApi, tarefa, { headers: this.jsonHeaders });
-  }
-
-
-  obterTarefa(id: string): Observable<Tarefa> {
-    if (id === '') {
-      return of(this.inicializarTarefa());
-    }
-    const urlID = `${this.urlApi}/${id}`;
-    return this.http.get<Tarefa>(urlID);
-
-  }
-
-  atualizarTarefa(tarefa: Tarefa) {
-    const urlID = `${this.urlApi}/${tarefa.id}`;
-    return this.http.put<Tarefa>(urlID, tarefa,
-      { headers: this.jsonHeaders }
-    );
-
-
-  }
-  excluirTarefa(id: string) {
-    const urlID = `${this.urlApi}/${id}`;
-    return this.http.delete<Tarefa>(urlID,
-      { headers: this.jsonHeaders }
-    );
-  }
-
-  private inicializarTarefa(): Tarefa {
-    return {
-      id: undefined,
-      nome: undefined,
-      detalhes: undefined,
-      concluido: undefined
-    }
-  }
-
-  private tratarErro(err: any) {
+  private handleError(err: any) {
     let msgError: string;
 
     if (err.error instanceof ErrorEvent) {
-      msgError = `Ocorreu um erro: ${err.error.message}`;
+      msgError = `An Error had ocurred ${err.error.message}`;
     } else {
-      msgError = `Ocorreu um error na api: ${err.staus}`;
+      msgError = `An Error had ocurred on the backend API: ${err.staus}`;
     }
 
     return new Error(msgError);
